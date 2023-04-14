270020bbd8ae87870f6464458d3eb4f26c8bbc42
LeaClover <plesen_14@mail.ru>
2023-04-14 12:05:27 +0300

    add animal service interface file

---
+++ interfaces/IAnimalService.cs
@@ -0,0 +1,8 @@
+﻿namespace FinalWork {
+    public interface IAnimalService {
+        void addAnimal(Animal animal);
+        List<Animal> getAllAnimals();
+        List<Command> showAnimalCommands(Animal animal);
+        void trainNewCommand(Animal animal, Command command);
+    }
+}
