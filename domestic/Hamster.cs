551b4deb03523aed27c4ec8635c9936063d346ed
LeaClover <plesen_14@mail.ru>
2023-04-14 12:05:07 +0300

    add hamster file

---
+++ domestic/Hamster.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalWork {
+
+    public class Hamster : DomesticAnimal
+    {
+        public Hamster(long id, string name, int age, string voice, Type type, Kind kind, List<Command> commandList) 
+            : base(id, name, age, voice, type, kind, commandList) {
+                this.kind = Kind.HAMSTER;
+        }
+    }
+}
