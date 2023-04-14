ba94a9d54345c5ffce0cc8bb041fe5f55f333f66
LeaClover <plesen_14@mail.ru>
2023-04-14 12:04:29 +0300

    add cat file

---
+++ domestic/Cat.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalWork {
+
+    public class Cat : DomesticAnimal
+    {
+        public Cat(long id, string name, int age, string voice, Type type, Kind kind, List<Command> commandList) 
+            : base(id, name, age, voice, type, kind, commandList) {
+                this.kind = Kind.CAT;
+        }
+    }
+}
