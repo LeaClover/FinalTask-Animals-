67c3a05f3873c9e0024f6f9a4d0685e80ba2ba91
LeaClover <plesen_14@mail.ru>
2023-04-14 12:02:52 +0300

    add kind file

---
+++ data/Kind.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalWork {
+    public enum Kind {
+        NONE,
+        CAT = 1,
+        DOG = 2,
+        HAMSTER = 3,
+        HORSE = 4,
+        DONKEY = 5,
+        CAMEL = 6
+    }
+}
