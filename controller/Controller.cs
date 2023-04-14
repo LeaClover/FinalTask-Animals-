249b6e9ca476be9c6ab6ce2de2e729e2504587d0
LeaClover <plesen_14@mail.ru>
2023-04-14 12:01:52 +0300

    add controller file

---
+++ controller/Controller.cs
@@ -0,0 +1,26 @@
+﻿namespace FinalWork {
+    public class Controller {
+        private AnimalService animalS = new AnimalService();
+        private AnimalView animalV = new AnimalView();
+
+        public void addAnimal(Animal animal) {
+            animalS.addAnimal(animal);
+        }
+
+        public void showAllAnimals() {
+            animalV.sendOnConsole(animalS.getAllAnimals());
+        }
+
+        public void showAllCommands(Animal animal) {
+            animalV.sendOnConsoleAnimalComands(animal);
+        }
+
+        public void addNewComand(Animal animal, Command command) {
+            animalS.trainNewCommand(animal, command);
+        }
+
+        public List<Animal> getAllAnimals() {
+            return animalS.animalList;
+        }
+    }
+}
