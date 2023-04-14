bf865e6e14c3a0a4036040391a86c09da921eb17
LeaClover <plesen_14@mail.ru>
2023-04-14 12:08:01 +0300

    add domestic animal service file

---
+++ service/DomesticAnimalService.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalWork {
+
+    public  class DomesticAnimalService : ICare, IVoice {
+
+        public void caress() {
+            Console.WriteLine("Меня погладили. Меня любят! <3");
+        }
+
+        public void eat() {
+            Console.WriteLine("Меня покормили");
+        }
+
+        public void voice(Animal animal) {
+            Console.WriteLine(animal.getVoice());
+        }
+    }
+}
