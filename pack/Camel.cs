addbc6dde631689ef357b58374b6219cc4b66b80
LeaClover <plesen_14@mail.ru>
2023-04-14 12:06:28 +0300

    add camel file

---
+++ pack/Camel.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalWork {
+
+    public class Camel : PackAnimal
+    {
+        public Camel(long id, string name, int age, string voice, Type type, Kind kind, List<Command> commandList) 
+            : base(id, name, age, voice, type, kind, commandList) {
+                this.kind = Kind.CAMEL;
+        }
+    }
+}
