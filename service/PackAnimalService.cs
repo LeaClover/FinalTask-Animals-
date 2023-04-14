5282e19f6430574b7b713d86afe9b85fbe43367f
LeaClover <plesen_14@mail.ru>
2023-04-14 12:08:15 +0300

    add pack animal service file

---
+++ service/PackAnimalService.cs
@@ -0,0 +1,21 @@
+﻿namespace FinalWork {
+
+    public class PackAnimalService : ICare, IVoice, ICarry {
+
+        public void caress() {
+            Console.WriteLine("Меня погладили. Меня любят! <3");
+        }
+
+        public void carry() {
+            Console.WriteLine("Я могу перевозить людей и поклажу!");
+        }
+
+        public void eat() {
+            Console.WriteLine("Меня покормили");
+        }
+
+        public void voice(Animal animal) {
+            Console.WriteLine(animal.getVoice());
+        }
+    }
+}
