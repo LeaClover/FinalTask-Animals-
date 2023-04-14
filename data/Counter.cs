8b5b305398ca93fbff10b22725078a21274493c1
LeaClover <plesen_14@mail.ru>
2023-04-14 12:02:38 +0300

    add counter file

---
+++ data/Counter.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalWork {
+
+    public class Counter : IDisposable {
+
+        public int sum; 
+        public Counter() {
+            this.sum = 0;
+        }
+        public void add() {
+            sum++;
+        }
+
+        public void Dispose() {
+            Console.WriteLine("Counter closed!");
+        }
+    }
+}
