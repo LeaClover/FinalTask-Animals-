624f10d08f37b90925a86fdd3efc1f0ac695ad16
LeaClover <plesen_14@mail.ru>
2023-04-14 12:07:48 +0300

    add animal service file

---
+++ service/AnimalService.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalWork {
+    public class AnimalService : IAnimalService {
+        public List<Animal> animalList;
+
+        public AnimalService() {
+            this.animalList = new List<Animal>{};
+        }
+
+        public void addAnimal(Animal animal) {
+            this.animalList.Add(animal);
+        }
+
+        public List<Animal> getAllAnimals() {
+            return this.animalList;
+        }
+
+        public List<Command> showAnimalCommands(Animal animal) {
+            return animal.commandList;
+        }
+
+        public void trainNewCommand(Animal animal, Command command) {
+            animal.commandList.Add(command);
+        }
+    }
+}
