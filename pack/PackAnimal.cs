23b147ea721805639592e2085e16d42849464741
LeaClover <plesen_14@mail.ru>
2023-04-14 12:07:31 +0300

    add pack animal file

---
+++ pack/PackAnimal.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalWork {
+
+    public class PackAnimal : Animal {
+        public PackAnimal(long id, string name, int age, string voice, Type type, Kind kind, List<Command> commandList) 
+            : base(id, name, age, voice, type, kind, commandList) {
+
+            this.type = Type.PACK;
+        }
+    }
+}
