75031065e81ed1fa48b0c3068da4d9f6be603ea4
LeaClover <plesen_14@mail.ru>
2023-04-14 12:06:40 +0300

    add donkey file

---
+++ pack/Donkey.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalWork {
+
+    public class Donkey : PackAnimal
+    {
+        public Donkey(long id, string name, int age, string voice, Type type, Kind kind, List<Command> commandList) 
+            : base(id, name, age, voice, type, kind, commandList) {
+                this.kind = Kind.DONKEY;
+        }
+    }
+}
